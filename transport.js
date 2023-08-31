5bf65ec56bc47f6676561e2d019701aaf3e98196
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-09-01 02:37:18 +0700

    update learn js

--- transport.js
+++ transport.js
@@ -1,6 +1,27 @@
-let amountTransport = prompt("Input a number Transport :");
+// case 1
+// let amountTransport = prompt("Input a number Transport :");
+// let transport = 1;
+// while (transport <= amountTransport) {
+//   console.log("Kendaraan No. " + transport + " beroperasi dengan baik.");
+//   transport++;
+// }
+
+// case 2
+// let jumlahTransport = prompt("Input a number of Transport :");
+// let transportAktif = prompt("Input a number Operation Transport (Active) : ");
+let jumlahTransport = 10;
+let transportAktif = 6;
 let transport = 1;
-while (transport <= amountTransport) {
-  console.log("Angkot No. " + transport + " beroperasi dengan baik.");
+
+while (transport <= transportAktif) {
+  console.log("Kendaraan No. " + transport + " beroperasi dengan baik.");
   transport++;
+}
+
+for (
+  transport = transportAktif + 1;
+  transport <= jumlahTransport;
+  transport++
+) {
+  console.log("Kendaraan No. " + transport + " sedang tidak beroperasi.");
 }
