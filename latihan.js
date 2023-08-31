18e55cc3baa8e433407c230e788f791c06e8fc95
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-08-31 16:08:37 +0700

    update learn

--- latihan.js
+++ latihan.js
@@ -1,1 +1,51 @@
-alert("Hello Wezup!");
+// alert("Hello Wezup!");
+
+// opeator comparison/perbandingan
+// contoh sederhana
+// console.log("Adhitya Hasan" === "Adhitya Hasan"); //true
+
+// contoh bil genap
+// let x = 5;
+
+// if (x % 2 == 0 && x <= 10) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+//operator penggabungan string
+// let a = "apple";
+// let b = "pen";
+// console.log(a + " " + b);
+
+// penjumlahan number dengan string
+// console.log(10 + 10 + "100"); //20100 (number + number + string = number & string)
+// console.log("10" + 10 + 10); //101010 (string + number + number = string)
+
+// typeof | cek tipe data
+// console.log(typeof 10);
+// console.log(typeof "10");
+// console.log(typeof true);
+
+// operator ternary
+// (kondisi) ? "benar" : "salah"
+// (x % 2 == 0) ? "genap" : "ganjil"
+
+// penggunaan kutip string
+// console.log('"Gerakan Mahasiswa" dilakukan pada hari Senin'); //masih benar
+//  // console.log('"Gerakan Mahasiswa" dilakukan pada hari jum'at '); //tidak jalan error
+// console.log('"Gerakan Mahasiswa" dilakukan pada hari jum\'at '); //tambahkan backslash sebelum kutip | true
+// console.log("satu \n backslash n");
+// console.log("dua \n\n dua backslash n");
+// console.log("coba backslash \t tab");
+
+// unicode
+// console.log("\u00A9"); //copyright
+// console.log("\u00AE"); //icon R
+// console.log("\u00B1"); //icon lebih kurang
+// console.log("\u00B5"); //icon u
+// console.log("\u2122"); //icon t mark
+
+// length
+// let firstName = "Adhitya Hasan";
+// console.log(firstName.length); // 13 karakter | .length start hitung dari angka 1,2,3,4... bukan dari 0,1,2,3...
