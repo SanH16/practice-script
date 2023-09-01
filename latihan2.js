a60cbd60f15667b9e4c331e02cd822da0d10f0c1
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-09-01 15:12:52 +0700

    update learn js

---
+++ latihan2.js
@@ -0,0 +1,11 @@
+// let a = 8;
+// let b = 3;
+// let volumeA;
+// let volumeB;
+// let total;
+
+// volumeA = a * a * a;
+// volumeB = b * b * b;
+
+// total = volumeA + volumeB;
+// console.log(total);
