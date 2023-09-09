77af63656224c11990022cedb3c30ed99dddcbdf
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-09-10 00:09:47 +0700

    update learn js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -29,3 +29,44 @@ while (num <= 30) {
   }
   num++; // incerement : num = num + 1;
 }
+
+// ======= Ganjil genap =======
+
+let angka = 10; // input berapapun, bisa pake prompt juga, console nya diganti alert
+if (angka % 2 == 0) {
+  console.log(`(${angka}) Angka ini adalah bilangan Genap`);
+} else {
+  console.log(`(${angka}) Angka ini adalah bilangan Ganjil`);
+}
+
+// contoh pake prompt
+
+// let angka2 = prompt("Masukka angka yang ini dicari : ");
+// if (angka2 % 2 == 0) {
+//   alert(`(${angka2}) Angka ini adalah bilangan Genap`);
+// } else {
+//   alert(`(${angka2}) Angka ini adalah bilangan Ganjil`);
+// }
+
+// ======= Identifikasi String yang muncul berapa kali =======
+
+// cara 1 😂
+const inputString = "Contoh karakter yang mau dihitung";
+console.log(inputString.length);
+
+// cara 2 | pake function
+
+const inputChar = "Contoh karakter yang mau dihitung"; // bebas apa aja
+
+function denganParameter(e) {
+  return e.length;
+}
+hitung = jumlahKarakter(inputChar);
+
+function tanpaParameter() {
+  counting = inputChar.length;
+  return counting;
+}
+
+console.log(`Jumlah karakter dalam string tersebut adalah (Fungsi Parameter) : ${hitung}`);
+console.log(`Jumlah karakter dalam string tersebut adalah (Fungsi !Parameter) : ${tanpaParameter()}`);
