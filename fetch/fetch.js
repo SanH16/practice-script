3f2f2e13cdab07d5b6cc93645081448792c7b1b6
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-12-19 14:48:20 +0700

    update learning fetch api

---
+++ fetch/fetch.js
@@ -0,0 +1,39 @@
+const endpoint = "https://reqres.in/api/users";
+
+//  fetch api
+// fetch(endpoint)
+//   .then((result) => result.json())
+//   .then(({ data }) => console.log(data));
+
+// async function
+const hitApi = async () => {
+  const result = await fetch(endpoint);
+  const { data } = await result.json();
+
+  console.log(data);
+
+  data.forEach((user) => {
+    const fetchElement = document.createElement("div");
+    fetchElement.classList.add("users");
+
+    const firstName = document.createElement("p");
+    firstName.innerText = user.first_name;
+
+    const lastName = document.createElement("span");
+    lastName.innerText = ` ${user.last_name}`;
+    lastName.style.color = "#BF3100";
+
+    const emailElement = document.createElement("p");
+    emailElement.innerText = user.email;
+    emailElement.style.color = "#087E8B";
+    emailElement.style.fontWeight = 500;
+
+    fetchElement.appendChild(firstName);
+    firstName.appendChild(lastName);
+    fetchElement.appendChild(emailElement);
+
+    document.getElementById("fetch-api").appendChild(fetchElement);
+  });
+};
+
+hitApi();
